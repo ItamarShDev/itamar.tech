c904e8399f46af68f0b2e2061ef5b7ab84ba0b31
Itamar Sharify <itamarsharify@gmail.com>
2021-10-30 09:54:55 +0200

    new: enable server components

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const withPlugins = require("next-compose-plugins");
 module.exports = withPlugins([], {
     experimental: {
         jsconfigPaths: true,
+        serverComponents: true,
     },
     i18n: {
         locales: ["en"],
