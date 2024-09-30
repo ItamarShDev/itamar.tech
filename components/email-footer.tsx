f8f70567bd5acec8a7b5b5a9eecfc211ed675354
Itamar Sharify <itamarsharify@gmail.com>
2024-09-30 14:09:05 +0300

    fix: styling

--- components/email-footer.tsx
+++ components/email-footer.tsx
@@ -3,7 +3,6 @@ import styles from "./EmailFooter.module.css";
 const EmailFooter = ({ title, text }) => {
 	return (
 		<footer className={styles.emailFooter}>
-			<h2>{title}</h2>
 			<p>{text}</p>
 			<a href={`mailto:example@example.com?subject=${title}`}>Send Email</a>
 		</footer>
