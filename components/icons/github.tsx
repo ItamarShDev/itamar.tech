ff4db17352cfe0aa255e2e8ba3026b65c98580e5
Itamar Sharify <itamarsharify@gmail.com>
2024-10-05 21:03:31 +0300

    Refactor GitHub component, update styles, and reorganize global CSS.

--- components/icons/github.tsx
+++ components/icons/github.tsx
@@ -2,7 +2,6 @@ import styles from "./Github.module.css";
 
 const Link = () => {
 	return (
-		// biome-ignore lint/a11y/useAnchorContent: <explanation>
 		<a
 			href="https://github.com/ItamarShDev/personal-website"
 			target="_blank"
@@ -10,26 +9,11 @@ const Link = () => {
 			aria-label="View source on GitHub"
 			rel="noreferrer"
 		>
-			<svg
-				width="60"
-				height="60"
-				viewBox="0 0 250 250"
-				style={{
-					position: "absolute",
-					top: "1em",
-					border: 0,
-
-					transform: "scale(-1, 1) rotate(-45deg)",
-					fill: "transparent",
-					color: "var(--colors-headerText)",
-				}}
-				aria-hidden="true"
-			>
-				<path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z" />
+			<title>open github</title>
+			<svg width="30" height="30" viewBox="90 20 100 110" aria-hidden="true">
 				<path
 					d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2"
 					fill="currentColor"
-					style={{ transformOrigin: "130px 106px" }}
 					className={styles.octoArm}
 				/>
 				<path
