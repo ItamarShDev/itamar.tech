8397c29ceee3e4942b17fdb944aa3dce7e3f73cf
Itamar Sharify <itamarsharify@gmail.com>
2024-10-05 21:07:50 +0300

    Add useDirection hook and translation support to Header component.

--- layouts/header.tsx
+++ layouts/header.tsx
@@ -1,3 +1,4 @@
+import { useDirection, useTranslation } from "lib/hooks/useTranslation";
 import Link from "next/link";
 import GithubLogo from "../components/icons/github";
 import LanguageSelector from "../components/language-selector";
@@ -6,13 +7,27 @@ import styles from "./Header.module.css";
 
 const Header = (props) => {
 	const { title } = props;
+	const direction = useDirection();
+	const translation = useTranslation({
+		en: {
+			title: "Itamar Sharify",
+		},
+		he: {
+			title: "איתמר שריפי",
+		},
+	});
 	return (
-		<nav className={styles.nav}>
+		<nav className={styles.nav} dir="ltr">
 			<LanguageSelector />
 			<ThemedIcon />
-			<Link href="/" title="Click to go home" className={styles.link}>
+			<Link
+				href="/"
+				title="Click to go home"
+				className={styles.link}
+				dir={direction}
+			>
 				<>
-					<span className={styles.name}>Itamar Sharify</span>
+					<span className={styles.name}>{translation.title}</span>
 					{title && (
 						<span className={styles.divider}>&nbsp;&nbsp;›&nbsp;&nbsp;</span>
 					)}
--- lib/hooks/useTranslation.tsx
+++ lib/hooks/useTranslation.tsx
@@ -5,3 +5,7 @@ export function useTranslation(translations) {
 	if (!locale) return translations.en;
 	return translations[locale];
 }
+export function useDirection() {
+	const { locale } = useRouter();
+	return locale === "he" ? "rtl" : "ltr";
+}
