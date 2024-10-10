36dda284d1287c6b48f507f3e9f7d23fdb1e8d0e
Itamar Sharify <itamarsharify@gmail.com>
2024-10-11 00:27:57 +0300

    fix: change home bg

--- layouts/home-background.tsx
+++ layouts/home-background.tsx
@@ -1,22 +1,35 @@
 import styles from "./HomeBackground.module.css";
 
-function getNarrowAngle() {
-	const degree = Math.random() * 360;
+function getNarrowAngle(baseDegree: number) {
+	const randomDegree = Math.random() * 360;
+	let degree = baseDegree + randomDegree;
+	if (Math.random() > 0.8) degree = baseDegree - randomDegree;
 	if (degree % 60 > 30) return degree + 30;
 	return degree;
 }
 
 function HomeBackground() {
-	const randomDegree = getNarrowAngle();
-	const bgStyle = {
-		backgroundImage: `linear-gradient(-${randomDegree}deg, var(--colors-bg) 50%, var(--colors-hoverDecorations) 50%)`,
-	};
-
+	const baseDegree = Math.random() * 360;
 	return (
 		<>
-			<div className={styles.bg} style={bgStyle} />
-			<div className={`${styles.bg} ${styles.bg2}`} style={bgStyle} />
-			<div className={`${styles.bg} ${styles.bg3}`} style={bgStyle} />
+			<div
+				className={styles.bg}
+				style={{
+					backgroundImage: `linear-gradient(-${getNarrowAngle(baseDegree)}deg, var(--colors-bg) 50%, var(--colors-hoverDecorations) 50%)`,
+				}}
+			/>
+			<div
+				className={`${styles.bg} ${styles.bg2}`}
+				style={{
+					backgroundImage: `linear-gradient(-${getNarrowAngle(baseDegree)}deg, var(--colors-bg) 50%, var(--colors-hoverDecorations) 50%)`,
+				}}
+			/>
+			<div
+				className={`${styles.bg} ${styles.bg3}`}
+				style={{
+					backgroundImage: `linear-gradient(-${getNarrowAngle(baseDegree)}deg, var(--colors-bg) 50%, var(--colors-hoverDecorations) 50%)`,
+				}}
+			/>
 		</>
 	);
 }
