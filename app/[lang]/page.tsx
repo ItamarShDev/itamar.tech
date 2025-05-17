66f86eef894bd1f706dd38a9f901558f16403cbc
Itamar Sharify <itamarsharify@gmail.com>
2025-05-17 22:08:58 +0300

    chore: add page metadata and disable anchor content a11y rule

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,14 +1,19 @@
 import HomeBackground from "layouts/home-background";
 import HomePage from "layouts/home-page";
 import { getFromKV } from "lib/get-data-methods";
+export const metadata = {
+  title: {
+    default: "Itamar Sharify",
+  },
+};
 
 export default async function Home() {
-	const quotes = await getFromKV("quotes");
+  const quotes = await getFromKV("quotes");
 
-	return (
-		<>
-			<HomeBackground />
-			<HomePage quotes={quotes?.en} />
-		</>
-	);
+  return (
+    <>
+      <HomeBackground />
+      <HomePage quotes={quotes?.en} />
+    </>
+  );
 }
