56d3a305f9a7f7623ba8833710b0f458bf959f2c
Itamar Sharify <itamarsharify@gmail.com>
2025-10-29 23:49:12 +0200

    fix: update Fireworks component to enforce percentage prop type for better type safety

--- components/match-finder/fireworks.tsx
+++ components/match-finder/fireworks.tsx
@@ -2,7 +2,7 @@
 
 import styles from "./Fireworks.module.css";
 
-function Fireworks(props) {
+function Fireworks(props: { percentage: number }) {
 	return (
 		<div className={props.percentage >= 90 ? styles.pyro : ""}>
 			<div className={styles.before} />
